a79f12ce63bdfad1af573ad371401fc0c1b60b7e
worthyadrenal <worthy-adrenal0t@icloud.com>
2025-04-26 22:48:25 +1000

    improve navigation

--- src/javascripts/crosswords/hidden-input.jsx
+++ src/javascripts/crosswords/hidden-input.jsx
@@ -3,93 +3,88 @@ import React, { Component } from 'react';
 class HiddenInput extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: this.props.value,
-    };
+    this.state = { value: '' };
   }
 
-  onClick(event) {
-    const cw = this.props.crossword || {};
-    if (typeof cw.onClickHiddenInput === 'function') {
-      cw.onClickHiddenInput(event);
-    }
-  }
+  onClick = (event) => {
+    this.props.crossword.onClickHiddenInput(event);
+  };
 
-  onKeyDown(event) {
-    const cw = this.props.crossword || {};
-    if (typeof cw.onKeyDown === 'function') {
-      cw.onKeyDown(event);
+  onKeyDown = (event) => {
+    const { cellInFocus, directionOfEntry } = this.props.crossword.state;
+    if (event.key === 'Backspace' || event.key === 'Delete') {
+      event.preventDefault();
+      if (cellInFocus && directionOfEntry) {
+        const { x, y } = cellInFocus;
+        const dx = directionOfEntry === 'across' ? -1 : 0;
+        const dy = directionOfEntry === 'down' ? -1 : 0;
+        const prevX = x + dx;
+        const prevY = y + dy;
+        // Clear previous cell
+        this.props.crossword.onMove({ x: prevX, y: prevY, value: '' });
+        // Focus that cell
+        this.props.crossword.focusClue(prevX, prevY, directionOfEntry);
+      }
+    } else {
+      this.props.crossword.onKeyDown(event);
     }
-  }
+  };
 
-  onBlur(event) {
-    const cw = this.props.crossword || {};
-    if (typeof cw.goToReturnPosition === 'function') {
-      cw.goToReturnPosition(event);
-    }
-  }
+  onBlur = (event) => {
+    this.props.crossword.goToReturnPosition(event);
+  };
 
-  onFocusPrevious() {
-    const cw = this.props.crossword || {};
-    if (typeof cw.focusPreviousClue === 'function') {
-      cw.focusPreviousClue();
-    }
-  }
+  onFocusPrevious = () => {
+    this.props.crossword.focusPreviousClue();
+  };
 
-  onFocusNext() {
-    const cw = this.props.crossword || {};
-    if (typeof cw.focusNextClue === 'function') {
-      cw.focusNextClue();
-    }
-  }
+  onFocusNext = () => {
+    this.props.crossword.focusNextClue();
+  };
 
-  touchStart(event) {
-    const cw = this.props.crossword || {};
-    if (typeof cw.onClickHiddenInput === 'function') {
-      cw.onClickHiddenInput(event);
-    }
-  }
+  touchStart = (event) => {
+    this.props.crossword.onClickHiddenInput(event);
+  };
 
-  handleChange(event) {
-    const cw = this.props.crossword || {};
-    if (typeof cw.insertCharacter === 'function') {
-      cw.insertCharacter(event.target.value);
+  handleChange = (event) => {
+    // Transform to uppercase
+    const char = event.target.value.toUpperCase();
+    // Insert into crossword
+    this.props.crossword.insertCharacter(char);
+    // Reset input
+    this.setState({ value: '' });
+    // Move to next cell automatically
+    const { cellInFocus, directionOfEntry } = this.props.crossword.state;
+    if (cellInFocus && directionOfEntry) {
+      const { x, y } = cellInFocus;
+      const dx = directionOfEntry === 'across' ? 1 : 0;
+      const dy = directionOfEntry === 'down' ? 1 : 0;
+      this.props.crossword.focusClue(x + dx, y + dy, directionOfEntry);
     }
-    this.setState({
-      value: '',
-    });
-  }
-
-  focusHiddenInput(x, y) {
-    if (this.input) {
-      this.input.focus();
-    }
-  }
+  };
 
   render() {
     return (
       <div
         className="crossword__hidden-input-wrapper"
-        ref={(wrapper) => {
-          this.wrapper = wrapper;
-        }}
+        ref={(wrapper) => { this.wrapper = wrapper; }}
       >
         <input
-          key="1"
+          key="prev"
           type="text"
           className="crossword__hidden-input-prev-next"
-          onFocus={this.onFocusPrevious.bind(this)}
+          onFocus={this.onFocusPrevious}
         />
         <input
-          key="2"
+          key="main"
           type="text"
           className="crossword__hidden-input"
           maxLength="1"
-          onClick={this.onClick.bind(this)}
-          onChange={this.handleChange.bind(this)}
-          onTouchStart={this.touchStart.bind(this)}
-          onKeyDown={this.onKeyDown.bind(this)}
-          onBlur={this.onBlur.bind(this)}
+          onClick={this.onClick}
+          onChange={this.handleChange}
+          onTouchStart={this.touchStart}
+          onKeyDown={this.onKeyDown}
+          onBlur={this.onBlur}
           value={this.state.value}
           autoComplete="off"
           spellCheck="false"
@@ -97,10 +92,10 @@ class HiddenInput extends Component {
           ref={(input) => { this.input = input; }}
         />
         <input
-          key="3"
+          key="next"
           type="text"
           className="crossword__hidden-input-prev-next"
-          onFocus={this.onFocusNext.bind(this)}
+          onFocus={this.onFocusNext}
         />
       </div>
     );
